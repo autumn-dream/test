0c8b3a19a4f26bde223d1c84668965f87e4aa3ec
autumn-dream <62228622+autumn-dream@users.noreply.github.com>
2020-03-16 12:16:52 +0800

    第一次上传
    第一次上传

---
+++ 江南牛逼游戏.py
@@ -0,0 +1,20 @@
+"""
+print("..........我爱江南.......")
+temp = input("江南牛逼吗？")
+guess = int(temp)
+if guess == 8:
+
+    print("是的，江南牛逼")
+else:
+
+    print("不，江南牛逼")
+""""
+
+print("..........我爱江南.......")
+temp = input("江南牛逼吗？")
+
+if temp == yes:
+    print("shide ")
+
+else:
+    print("no")
